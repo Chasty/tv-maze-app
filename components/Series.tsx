11195e32d75266eec5498681e23693e38060a996
Chasty <coderessence@gmail.com>
2023-07-21 19:01:54 -0500

    feat: add empty state

--- components/Series.tsx
+++ components/Series.tsx
@@ -1,7 +1,7 @@
-import { StyleSheet, FlatList } from "react-native";
+import { StyleSheet, FlatList, Image } from "react-native";
 import React from "react";
 import { Serie, SerieProps } from "./Serie";
-import { View } from "./Themed";
+import { Text, View } from "./Themed";
 
 type SeriesProps = {
   series: Array<SerieProps>;
@@ -20,7 +20,19 @@ const Series = ({ series }: SeriesProps) => {
         }}
         data={series}
         renderItem={renderItem}
-        keyExtractor={(prop, index) => `${prop.id}$-{index}`}
+        keyExtractor={(prop, index) => `${prop.id}-${index}`}
+        ListEmptyComponent={() => (
+          <View style={styles.emptyContainer}>
+            <Image
+              style={styles.emptyImage}
+              source={require("../assets/images/no-images-found.jpg")}
+            />
+            <Text style={styles.text}>No series found</Text>
+            <Text style={styles.text}>
+              Try to search a serie by another name.
+            </Text>
+          </View>
+        )}
       />
     </View>
   );
@@ -31,5 +43,17 @@ export default Series;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  emptyImage: {
+    width: 300,
+    height: 300,
+  },
+  emptyContainer: {
+    marginTop: 80,
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 15,
+    marginTop: 8,
   },
 });
